b3a0d35a204659824a603a82c06c759780b33f9b
LLM4VD <wyk989@gmail.com>
2024-06-15 14:15:06 +0800

    Delete run.sh

--- run.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-python infer/qwen_line.py --type qlora --tp basic &&
-python infer/qwen_line.py --type qlora --tp basic2 &&
-python infer/qwen_line.py --type qlora --tp cot &&
-python infer/qwen_line.py --type qlora --tp cot2
-
-
